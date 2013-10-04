aef301dbaa4424057611d2133481820cf492f8ba
omatsola sobotie <tsola2002@yahoo.co.uk>
2013-10-04 12:57:43 +0100

    bare bones active record setup for dashboard website project

---
+++ application/controllers/hello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * Package: dashboard
+ * Filename: hello.php
+ * Author: solidstunna101
+ * Date: 04/10/13
+ * Time: 10:06
+ *
+ */
+
+class hello extends CI_Controller {
+
+
+}
---
+++ application/models/Tests.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * Package: dashboard
+ * Filename: Tests.php
+ * Author: solidstunna101
+ * Date: 04/10/13
+ * Time: 10:22
+ *
+ */
+
+class Tests extends ActiveRecord\Model {
+
+
+}
